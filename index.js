5402d37f3c1f7f06986e1edf42582e5d4abafeba
J-haug <jeremiahhaugan@gmail.com>
2025-09-02 13:08:18 -0500

    added item sort

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const itemList = document.querySelector('.items__container')
-
+let items
 
 async function getAPI() {
     const objects = await fetch('https://fakestoreapi.com/products/')
     const objectsData = await objects.json()
     console.log(objectsData)
-    itemList.innerHTML = objectsData.map((object) =>
+    return objectsData
+    
+}
+
+async function renderItems(filter){
+    if (!items){
+        items = await getAPI()
+    }
+    if (filter === 'highest-rated'){
+        items.sort((a,b) => b.rating.rate - a.rating.rate)
+    }
+    else if (filter === 'price-low-to-high'){
+        items.sort((a,b) => (a.price - b.price))
+    }
+    else if (filter === 'price-high-to-low'){
+        items.sort((a,b) => (b.price - a.price))
+    }
+
+    const itemHTML = items.map((object) =>
         `<div class="item">
             <figure class="item__img--wrapper">
                 <img class="item__img" src="${object.image}" alt="">
@@ -15,17 +33,25 @@ async function getAPI() {
                 <div class="item__sub-info">
                     <h2 class="price">$${object.price}</h2>
                     <div class="stars">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
+                        ${ratingsHTML(object.rating)}
                     </div>
                 </div>
             </div>
         </div>`).join('')
+        itemList.innerHTML = itemHTML
 }
 
+function ratingsHTML(rating){
+    let ratingHTML = ''
+    for (let i = 0; i < Math.floor(rating.rate); i++){
+        ratingHTML +=`<i class="fa-solid fa-star"></i>`
+    }
+    if (!Number.isInteger(rating.rate)){
+        ratingHTML +=`<i class="fa-solid fa-star-half"></i>`
+    }
+    return ratingHTML
+}
 
-
-getAPI()
+function filterItems(event){
+    renderItems(event.target.value)
+}
