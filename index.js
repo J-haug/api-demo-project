9609cae4d120d223859fe722d4242a7f886e1423
J-haug <jeremiahhaugan@gmail.com>
2025-09-03 19:13:26 -0500

    fixes

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
 const itemList = document.querySelector('.items__container')
+let allItems = null
 let items
+
+const sortEl = document.querySelector('.filter__dropdown')
+const categoryEl = document.querySelector('.category__dropdown')
 const loadingScreen = document.querySelector('.loading-screen')
 
 async function getAPI() {
     const objects = await fetch('https://fakestoreapi.com/products/')
     const objectsData = await objects.json()
     console.log(objectsData)
+   
     return objectsData
     
 }
 
 async function update() {
     loadingScreen.classList.add('visible')
-    items = await getAPI()
+    if (!allItems){
+        allItems = await getAPI()
+    }
+    items = [...allItems]
 
-    if (document.querySelector('.category__dropdown').value === 'all'){
-        items = await getAPI()
-    }
-    else if (document.querySelector('.category__dropdown').value === 'electronics'){
-        items = items.filter(item => item.category === "electronics")
-    }
-    else if (document.querySelector('.category__dropdown').value === "men's clothing"){
-        items = items.filter(item => item.category === "men's clothing")
-    }
-    else if (document.querySelector('.category__dropdown').value === "women's clothing"){
-        items = items.filter(item => item.category === "women's clothing")
-    }
-    else if (document.querySelector('.category__dropdown').value === "jewelery"){
-        items = items.filter(item => item.category === "jewelery")
+    if (categoryEl.value !== 'all'){
+        if (categoryEl.value === 'electronics'){
+            items = items.filter(item => item.category === "electronics")
+        }
+        else if (categoryEl.value === "men's clothing"){
+            items = items.filter(item => item.category === "men's clothing")
+        }
+        else if (categoryEl.value === "women's clothing"){
+            items = items.filter(item => item.category === "women's clothing")
+        }
+        else if (categoryEl.value === "jewelery"){
+            items = items.filter(item => item.category === "jewelery")
+        }
     }
 
 
-    if (document.querySelector('.filter__dropdown').value === 'highest-rated'){
+    if (sortEl.value === 'highest-rated'){
         items.sort((a,b) => b.rating.rate - a.rating.rate)
     }
-    else if (document.querySelector('.filter__dropdown').value === 'price-low-to-high'){
+    else if (sortEl.value === 'price-low-to-high'){
         items.sort((a,b) => (a.price - b.price))
     }
-    else if (document.querySelector('.filter__dropdown').value === 'price-high-to-low'){
+    else if (sortEl.value === 'price-high-to-low'){
         items.sort((a,b) => (b.price - a.price))
         
     }
     renderItems()
+    console.log(items)
 }
 
 function renderItems(){
-    
     const itemHTML = items.map((object) =>
         `<div class="item">
             <figure class="item__img--wrapper">
@@ -79,9 +86,6 @@ function ratingsHTML(rating){
     return ratingHTML
 }
 
-function filterItems(event){
-    update()
-}
 
 
 update()
